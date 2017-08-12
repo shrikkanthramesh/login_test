469c747f3d2c8bfe2d0377cb4b5ec8dafcdbd624
Shrikkanth Ramesh <shrikkanth.ramesh@unicosolution.com>
2017-08-12 17:52:39 +0530

    changed to service

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   	private router: Router) { }
 
    sendRequest() {
-  	let url = "https://login.windows.net/common/oauth2/authorize?response_type=code%20id_token&scope=openid&response_mode=form_post&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Felasticetl%2Fapi%2Fv1%2Fhome&client_id=793a6e11-2f9b-47d8-bb68-baba7602fee8&resource=https%3a%2f%2fgraph.windows.net&nonce=83473c67-c535-42e2-a5b9-a6a0e3ba3ce1&site_id=500879";
+  	let url = "http://localhost:8080/elasticetl/api/v1/home";
   	let headers = new Headers({
   		'Content-Type' : 'application/json'
   	});
